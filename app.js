6df2a92372c0ec6ea014e7f505316e359b77b4ac
themchoise <86273107+themchoise@users.noreply.github.com>
2021-09-23 20:07:38 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-let listadoClientes = []
+let listadoClientes = [];
 let numbers = '^[0-9]+$';
 let inicio = () => {
     result = window.confirm('Todos los datos son Obligatorios\n¿Desea Igresar un nuevo cliente?');
-    return result
+    return result;
 }
 
 let continua = () => {
     result = window.confirm('¿Agrega otro cliente cliente?');
-    return result
+    return result;
 }
 
 let cargaCliente = () => {
     let nombre = prompt('Ingrese el nombre del Cliente');
     let razonSocial = prompt('Razon Social');
     let telefono = prompt('Telefono');
-    return [nombre, razonSocial, telefono]   
+    return [nombre, razonSocial, telefono];
 }
 
 if (inicio()) {
@@ -24,17 +24,25 @@ if (inicio()) {
     do { 
             const [nombre, razonSocial, telefono] = cargaCliente();
             if (nombre && razonSocial &&  telefono ){
-            let altaCliente = new Cliente(nombre, razonSocial, telefono)
-            listadoClientes.push(altaCliente)
+            let altaCliente = new Cliente(nombre, razonSocial, telefono);
+            listadoClientes.push(altaCliente);
                 }
             
     } while (continua());
 
-
 }
 
 if (listadoClientes.length > 0) {
-    alert('Resultado por Consola')
+
+    for (let i = 0; i < listadoClientes.length; i++) {
+        const element = listadoClientes[i];
+        let {nombre,id} = element        
+        localStorage.setItem(id, `Cliente: ${nombre}`)
+        
+    }
+
+
+    alert('Resultado por Consola y almacenados en LocalStorage');
     console.log('Su Listado de Clientes:');
     console.log(listadoClientes);
 }
